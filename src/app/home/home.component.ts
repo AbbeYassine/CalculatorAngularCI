eac6e17fc562dd0d4dc36d95da51a873691277b3
ABBES <abbesyassine4@gmail.com>
2017-01-17 21:06:05 +0100

    Fixing project structure

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,6 @@ import {
   OnInit
 } from '@angular/core';
 
-import { AppState } from '../app.service';
-import { Title } from './title';
-import { XLargeDirective } from './x-large';
 
 @Component({
   // The selector is what angular internally uses
@@ -13,31 +10,17 @@ import { XLargeDirective } from './x-large';
   // where, in this case, selector is the string 'home'
   selector: 'home',  // <home></home>
   // We need to tell Angular's Dependency Injection which providers are in our app.
-  providers: [
-    Title
-  ],
+  providers: [],
   // Our list of styles in our component. We may add more to compose many styles together
-  styleUrls: [ './home.component.css' ],
+  styleUrls: [],
   // Every Angular template is first compiled by the browser before Angular runs it's compiler
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
   // Set our default values
-  public localState = { value: '' };
-  // TypeScript public modifiers
-  constructor(
-    public appState: AppState,
-    public title: Title
-  ) {}
+
 
   public ngOnInit() {
-    console.log('hello `Home` component');
-    // this.title.getData().subscribe(data => this.data = data);
   }
 
-  public submitState(value: string) {
-    console.log('submitState', value);
-    this.appState.set('value', value);
-    this.localState.value = '';
-  }
 }
